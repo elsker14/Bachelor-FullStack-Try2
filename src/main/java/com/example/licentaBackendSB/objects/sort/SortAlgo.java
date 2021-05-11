6fcae11d94be6c8ba6050affeeaf9812707fc7ca
elsker14 <iancujianu98@gmail.com>
2021-05-11 14:42:11 +0300

    #1 commit : all project by now

---
+++ src/main/java/com/example/licentaBackendSB/objects/sort/SortAlgo.java
@@ -0,0 +1,31 @@
+package com.example.licentaBackendSB.objects.sort;
+
+public class SortAlgo {
+
+    //needed for Statistics source
+    private String sortingAlgoName;
+    private long duration;
+
+    public SortAlgo() { }
+
+    public SortAlgo(String sortingAlgoName, long duration) {
+        this.sortingAlgoName = sortingAlgoName;
+        this.duration = duration;
+    }
+
+    public String getSortingAlgoName() {
+        return sortingAlgoName;
+    }
+
+    public void setSortingAlgoName(String sortingAlgoName) {
+        this.sortingAlgoName = sortingAlgoName;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+}
