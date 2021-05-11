c4fd3dacf6973030227175e2851e2c7a5bebf622
elsker14 <iancujianu98@gmail.com>
2021-05-11 15:00:18 +0300

    #3 commit: made StudentController the controller to a certain address

--- src/main/java/com/example/licentaBackendSB/AccessingDataMysqlApplication.java
+++ src/main/java/com/example/licentaBackendSB/AccessingDataMysqlApplication.java
@@ -1,29 +1,12 @@
 package com.example.licentaBackendSB;
 
-import com.example.licentaBackendSB.objects.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class AccessingDataMysqlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccessingDataMysqlApplication.class, args);
 	}
-
-	@GetMapping
-	public List<Student> hello()
-	{
-		List<Student> studentsDB = new ArrayList<>();
-		studentsDB = Student.hardcodeStudents();
-
-		return studentsDB;
-	}
-
 }
--- src/main/java/com/example/licentaBackendSB/objects/student/StudentController.java
+++ src/main/java/com/example/licentaBackendSB/objects/student/StudentController.java
@@ -1,4 +1,22 @@
 package com.example.licentaBackendSB.objects.student;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/students")
 public class StudentController {
+
+    @GetMapping
+    public List<Student> getStudents()
+    {
+        List<Student> studentsDB = new ArrayList<>();
+        studentsDB = Student.hardcodeStudents();
+
+        return studentsDB;
+    }
 }
